84b27ec4ad691fc22e5021b91b0c5c036a2cc078
pblasiak <blasiak.przemek@gmail.com>
2023-08-10 23:48:49 +0200

    Chapter 12.7.4. - Function

--- PPP2/PPP2GUI/firstExample/main.cpp
+++ PPP2/PPP2GUI/firstExample/main.cpp
@@ -9,25 +9,45 @@
 
 //------------------------------------------------------------------------------
 
-int main()
-{
-    using namespace Graph_lib;   // our graphics facilities are in Graph_lib
+int main ()
+try
+{ 
+    Point tl(100,100);    // top-left corner of our window
 
-    Point tl(100,100);           // to become top left  corner of window
+    Simple_window win(tl,600,400,"Canvas");
+    // screen coordinate tl for top-left corner
+    // window size(600*400)
+    // title: Canvas
 
-    Simple_window win(tl,600,400,"Canvas");    // make a simple window
+    Axis xa(Axis::x, Point(20,300), 280, 10, "x axis"); // make an Axis
+    // an axis is a kind of Shape
+    // Axis::x means horizontal
+    // starting at (20,300)
+    // 280 pixels long
+    // 10 "notches"
+    // label the axis "x axis"
+    win.attach(xa);                      // attach xa to the window, win
 
-    Graph_lib::Polygon poly;                // make a shape (a polygon)
+    Axis ya(Axis::y, Point(20,300), 280, 10, "y axis");
+    ya.set_color(Color::cyan);           // choose a color
+    ya.label.set_color(Color::dark_red); // choose a color for the text
+    win.attach(ya);
 
-    poly.add(Point(300,200));    // add a point
-    poly.add(Point(350,100));    // add another point
-    poly.add(Point(400,200));    // add a third point 
+    Function sine(sin,0,100,Point(20,150),1000,50,50);    // sine curve
+    // plot sin() in the range [0:100) with (0,0) at (20,150)
+    // using 1000 points; scale x values *50, scale y values *50
+    win.attach(sine);
 
-    poly.set_color(Color::red);  // adjust properties of poly
-
-    win.attach (poly);           // connect poly to the window
-
-    win.wait_for_button();       // give control to the display engine
+    win.set_label("Canvas #4");
+    win.wait_for_button();               // Display!
+} 
+catch(exception& e) {
+    // some error reporting
+    return 1;
+}
+catch(...) {
+    // some more error reporting
+    return 2;
 }
 
 //------------------------------------------------------------------------------
