a934030f59d74f199215a9b34cc1fc9b1d4d29b5
pblasiak <blasiak.przemek@gmail.com>
2023-09-19 20:44:43 +0200

    Dodano rozwiazanie Drill z Chapter 13

--- PPP2/PPP2GUI/firstExample/main.cpp
+++ PPP2/PPP2GUI/firstExample/main.cpp
@@ -15,19 +15,40 @@ int main()
 
     Point tl(100,100);           // to become top left  corner of window
 
-    Simple_window win(tl,600,400,"Canvas");    // make a simple window
+    Simple_window win(tl,800,1000,"Chapter 13 - Drill");    // make a simple window
 
-    Graph_lib::Polygon poly;                // make a shape (a polygon)
+    constexpr int squareWidth = 100;
+    Vector_ref<Graph_lib::Rectangle> recs;
+    for (int i = 0; i < 8; ++i)
+    {
+        for (int j = 0; j < 8; ++j)
+        {
+            recs.push_back(new Graph_lib::Rectangle{Point{i * squareWidth,j * squareWidth}, squareWidth,squareWidth});
+            if (i == j)
+            {
+                recs[recs.size() - 1].set_fill_color(Color::dark_red);
+            }
+            win.attach(recs[recs.size()-1]);
+        }
+    }
+    // 4
+    Image im1{ Point{300,0}, "Chopin.jpg"};
+    Image im2{ Point{100,300}, "Chopin.jpg"};
+    Image im3{ Point{600,300}, "Chopin.jpg"};
+    win.attach(im1);
+    win.attach(im2);
+    win.attach(im3);
 
-    poly.add(Point(300,200));    // add a point
-    poly.add(Point(350,100));    // add another point
-    poly.add(Point(400,200));    // add a third point 
+    // 5
+    int s = 50;
+    Image im4{ Point{s,0}, "FCB.jpg"};
+    win.attach(im4);
 
-    poly.set_color(Color::red);  // adjust properties of poly
-
-    win.attach (poly);           // connect poly to the window
-
-    win.wait_for_button();       // give control to the display engine
+    for (int i = 0; i < 3; s+=100,++i)
+    {
+        im4.move(s, 0);
+        win.wait_for_button();       // give control to the display engine 
+    }
 }
 
 //------------------------------------------------------------------------------
